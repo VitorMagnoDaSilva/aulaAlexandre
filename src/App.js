c109613b813199aa783b4aa4048468d774c2580a
VitorMagnoDaSilva <magno.vitor132@gmail.com>
2020-09-29 20:42:07 -0300

    oi

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Projeto Redux</h1>
+
+        <div className="linha">
+          <Card title="Card 1" red> TESTE 1 </Card>
+        </div>
+
+        <div className="linha">
+        <Card title="Card 2" green> TESTE 2 </Card>
+        <Card title="Card 3" blue> TESTE 3 </Card>
+        <Card title="Card 4" purple> TESTE 4 </Card>
+        </div>
+
+
     </div>
   );
 }
